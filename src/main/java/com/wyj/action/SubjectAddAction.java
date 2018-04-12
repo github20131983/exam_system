d614b19bdfc756663f0fff0cd05407822889bbb9
github20131983 <32866135+github20131983@users.noreply.github.com>
2018-04-12 22:25:14 +0800

    添加题目Action

--- src/main/java/com/wyj/action/SubjectAddAction.java
+++ src/main/java/com/wyj/action/SubjectAddAction.java
@@ -14,6 +14,54 @@ public class SubjectAddAction extends ActionSupport {
 	private String subjectParse;
 	private String subjectTitle;
 	private SubjectService subjectService=new SubjectServiceImpl();
+	public String getSubjectAnswer() {
+		return subjectAnswer;
+	}
+	public void setSubjectAnswer(String subjectAnswer) {
+		this.subjectAnswer = subjectAnswer;
+	}
+	public String getSubjectOptionA() {
+		return subjectOptionA;
+	}
+	public void setSubjectOptionA(String subjectOptionA) {
+		this.subjectOptionA = subjectOptionA;
+	}
+	public String getSubjectOptionB() {
+		return subjectOptionB;
+	}
+	public void setSubjectOptionB(String subjectOptionB) {
+		this.subjectOptionB = subjectOptionB;
+	}
+	public String getSubjectOptionC() {
+		return subjectOptionC;
+	}
+	public void setSubjectOptionC(String subjectOptionC) {
+		this.subjectOptionC = subjectOptionC;
+	}
+	public String getSubjectOptionD() {
+		return subjectOptionD;
+	}
+	public void setSubjectOptionD(String subjectOptionD) {
+		this.subjectOptionD = subjectOptionD;
+	}
+	public String getSubjectParse() {
+		return subjectParse;
+	}
+	public void setSubjectParse(String subjectParse) {
+		this.subjectParse = subjectParse;
+	}
+	public String getSubjectTitle() {
+		return subjectTitle;
+	}
+	public void setSubjectTitle(String subjectTitle) {
+		this.subjectTitle = subjectTitle;
+	}
+	public SubjectService getSubjectService() {
+		return subjectService;
+	}
+	public void setSubjectService(SubjectService subjectService) {
+		this.subjectService = subjectService;
+	}
 	public String execute() throws Exception{
 		Subject subject=new Subject();
 		subject.setSubjectAnswer(subjectAnswer);
@@ -25,7 +73,8 @@ public class SubjectAddAction extends ActionSupport {
 		subject.setSubjectOptionD(subjectOptionD);
 		subject.setSubjectParse(subjectParse);
 		if(subjectService.saveSubject(subject)){
-			return SUCCESS;
+			System.out.println("成功");
+			return SUCCESS;			
 		}
 		else{
 			this.addActionError("该试题已经添加过，请不要重复添加！");
