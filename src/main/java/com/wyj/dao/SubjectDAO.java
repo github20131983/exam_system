76891d0d17e843c18b0b931f2ff41241cf992bf2
github20131983 <32866135+github20131983@users.noreply.github.com>
2018-04-13 22:54:49 +0800

    subjectDAO

--- src/main/java/com/wyj/dao/SubjectDAO.java
+++ src/main/java/com/wyj/dao/SubjectDAO.java
@@ -5,4 +5,7 @@ import com.wyj.po.Subject;
 public interface SubjectDAO {
 	public Subject findSubjectByTitle (String subjectTitle);//根据题目找题
 	public void addSubject(Subject subject);//添加题目
+	public Subject findSubjectByID(int subjectID);//根据ID找题
+	public void updateSubject(Subject subject);//更新题库
+	public void deleteSubject(int subjectID);
 }
