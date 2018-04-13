a98c84b506dd7f1715c46ca41719a42f71c23453
github20131983 <32866135+github20131983@users.noreply.github.com>
2018-04-13 22:53:54 +0800

    subject的service方法

--- src/main/java/com/wyj/service/SubjectService.java
+++ src/main/java/com/wyj/service/SubjectService.java
@@ -4,4 +4,7 @@ import com.wyj.po.Subject;
 
 public interface SubjectService {
 	public boolean saveSubject(Subject subject);//添加试题，返回是否成功
+	public Subject showParcularSubject(int subjectID);//查看试题
+	public void updateSubject(Subject subject);//更新试题
+	public void deleteSubject(int subjectID);//删除试题
 }
